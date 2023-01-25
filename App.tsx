d4c3403b8aeed5ea1a7f7773d8d5650bbd5f8a67
Manoel Duran <manoel.duran@hotmail.com>
2023-01-24 22:23:21 -0300

    refact(STYLE): changed native base to styled-components/native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
 import React from 'react';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from '@expo-google-fonts/inter';
-import { THEME } from './src/styles/theme';
 import Loading from './src/components/Loading';
 import { Provider } from "react-redux";
 import { store } from "./src/store";
 import Routes from "./src/routes";
-import Room from "./src/screens/Room";
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from 'expo-status-bar';
+import {theme} from './src/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,8 +16,8 @@ export default function App() {
   })
   return (
     <Provider store={store}>
-      <NativeBaseProvider theme={THEME}>
-        <StatusBar barStyle="light-content"
+      <ThemeProvider theme={theme}>
+        <StatusBar style='light'
           backgroundColor="transparent"
           translucent
         />
@@ -25,7 +25,7 @@ export default function App() {
           <Routes/>
           : <Loading />
         }
-      </NativeBaseProvider>
+      </ThemeProvider>
     </Provider>
   );
 }
