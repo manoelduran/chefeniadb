696459a39075402ccb1ba670293971c3a4bab03f
Manoel Duran <manoel.duran@hotmail.com>
2023-02-03 21:16:51 -0300

    feat(Mvp): created mvp screen

--- src/screens/Mvp/index.tsx
+++ src/screens/Mvp/index.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { useTheme } from 'styled-components/native';
 import { MvpNavigationProps } from '../../@types/navigation';
 import IconButton from '../../components/IconButton';
-import { Container, LogoutContainer, ScrollContainer, Title } from './styles';
+import { Container, GoBackContainer, ImageContainer, Separator, LogoutContainer, MvpImage, PropertiesContainer, ScrollContainer, Title, PropertiesText, WeaknessesContainer, LabelWeaknesses, DataContainer, ElementContainer } from './styles';
 const Mvp = () => {
     const route = useRoute();
     const { mvp } = route.params as MvpNavigationProps;
+    console.log('mvp', mvp)
     const navigation = useNavigation();
     const theme = useTheme();
     const goBack = () => {
@@ -15,12 +16,75 @@ const Mvp = () => {
     return (
         <Container>
             <ScrollContainer>
-            <LogoutContainer>
-                <Title>{mvp.name}</Title>
-                <IconButton onPress={goBack} color={theme.colors.gray[400]} icon='arrow-back' />
-            </LogoutContainer>
-                </ScrollContainer>
-                </Container>
+                <LogoutContainer>
+                    <GoBackContainer>
+                        <IconButton onPress={goBack} color={theme.colors.gray[400]} icon='arrow-back' />
+                    </GoBackContainer>
+                </LogoutContainer>
+                <ImageContainer>
+                    <MvpImage source={{ uri: mvp.mvp_url }} resizeMode="cover" alt="Room Map" />
+                    <Title>{mvp.name}</Title>
+                </ImageContainer>
+                <PropertiesContainer>
+                    <DataContainer>
+                        <PropertiesText>QUANTITY</PropertiesText>
+                        <PropertiesText>LEVEL</PropertiesText>
+                        <PropertiesText>BREED</PropertiesText>
+                        <PropertiesText>PROPERTY</PropertiesText>
+                    </DataContainer>
+                    <Separator />
+                    <DataContainer>
+                    <PropertiesText>{mvp.quantity}</PropertiesText>
+                        <PropertiesText>{mvp.level}</PropertiesText>
+                        <PropertiesText>{mvp.breed}</PropertiesText>
+                        <PropertiesText>{mvp.property}</PropertiesText>
+                    </DataContainer>
+                </PropertiesContainer>
+                <LabelWeaknesses>WEAKNESSES</LabelWeaknesses>
+                <WeaknessesContainer>
+                    <ElementContainer color='#C44D41' >
+                        <PropertiesText >FIRE</PropertiesText>
+                        <PropertiesText>{mvp.fire}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#CCB25B'>
+                        <PropertiesText>EARTH</PropertiesText>
+                        <PropertiesText>{mvp.earth}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#3B9698'>
+                        <PropertiesText>WIND</PropertiesText>
+                        <PropertiesText>{mvp.wind}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#265990'>
+                        <PropertiesText>WATER</PropertiesText>
+                        <PropertiesText>{mvp.water}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#38A649'>
+                        <PropertiesText>POISON</PropertiesText>
+                        <PropertiesText>{mvp.poison}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#6B48C7'>
+                        <PropertiesText>DARK</PropertiesText>
+                        <PropertiesText>{mvp.dark}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#B3B3BD'>
+                        <PropertiesText>NEUTRAL</PropertiesText>
+                        <PropertiesText>{mvp.neutral}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#E4E3E8'>
+                        <PropertiesText>HOLY</PropertiesText>
+                        <PropertiesText>{mvp.holy}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#C2A1D8'>
+                        <PropertiesText>GHOST</PropertiesText>
+                        <PropertiesText>{mvp.ghost}</PropertiesText>
+                    </ElementContainer>
+                    <ElementContainer color='#272733'>
+                        <PropertiesText>UNDEAD</PropertiesText>
+                        <PropertiesText>{mvp.undead}</PropertiesText>
+                    </ElementContainer>
+                </WeaknessesContainer>
+            </ScrollContainer>
+        </Container>
     );
 }
 
--- src/screens/Mvp/styles.ts
+++ src/screens/Mvp/styles.ts
@@ -1,7 +1,9 @@
 import { Form } from "@unform/mobile";
 import styled from "styled-components/native";
 
-
+interface ElementContainerProps  {
+    color?: string;
+}
 
 export const Container = styled.View`
 flex: 1;
@@ -23,17 +25,112 @@ justify-content: space-between;
 
 `;
 
+export const GoBackContainer = styled.View`
+background-color: #213790;
+mix-blend-mode: normal;
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+padding: 5px;
+border-radius: 8px;
+`;
+
+
 export const KeyBoardAvoidContainer = styled.KeyboardAvoidingView`
 
 `;
 
 export const ScrollContainer = styled.ScrollView``;
 
-export const Title = styled.Text`
-font-family: ${({theme}) => theme.fonts.primary_600};
-font-size: 25px;
-color: ${({theme}) => theme.colors.success[600]};
+export const ImageContainer = styled.View`
+margin-top: 20px;
+padding: 20px 0px;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-evenly;
 `;
 
-export const BackgroundImage = styled.Image`
+export const Title = styled.Text`
+//font-family: 'Archivo Black';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 22px;
+text-align: center;
+
+color: #FFFFFF;
+
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const MvpImage = styled.Image`
+width: 120px;
+height: 110px;
+`;
+
+export const PropertiesContainer = styled.View`
+margin-top: 20px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+background: #213791;
+padding: 20px;
+mix-blend-mode: normal;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 25px;
+`;
+
+export const DataContainer = styled.View`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
+export const Separator = styled.View`
+width: 4px;
+background: #7B8DD7;
+`;
+
+export const PropertiesText = styled.Text`
+//font-family: 'Archivo Black';
+margin-top: 25px;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 22px;
+text-align: center;
+
+color: #FFFFFF;
+
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const WeaknessesContainer = styled.View`
+margin-top: -10px;
+border-radius: 25px;
+`;
+
+export const LabelWeaknesses = styled.Text`
+//font-family: 'Archivo Black';
+z-index: 2;
+margin-top: 25px;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 22px;
+text-align: center;
+
+color: #FFFFFF;
+
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const ElementContainer = styled.View<ElementContainerProps>`
+
+display: flex;
+flex-direction: row;
+background: ${({color}) => color ? color : '#213791'};
+align-items: center;
+text-align: center;
+justify-content: space-around;
 `;
