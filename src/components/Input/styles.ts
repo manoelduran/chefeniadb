2c65e3ae526395b9a7b192bbd78890063dfff366
Manoel Duran <manoel.duran@hotmail.com>
2023-02-03 21:16:00 -0300

    fix(InputStyle): changed background and color

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,11 +2,13 @@ import styled, {css} from "styled-components/native";
 
 
 export const InputStyle = styled.TextInput`
-background-color: ${({theme}) => theme.colors.gray[600]};
-font-size: ${({theme}) => theme.fontSizes.sm};
+background: #213791;
+font-size: ${({theme}) => theme.fontSizes.md};
 border-width: 0;
-font-family: ${({theme}) => theme.fonts.primary_500};
-color: ${({theme}) => theme.colors.white};
+font-style: normal;
+line-height: 22px;
+color: #FFFFFF;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 padding: 10px;
 border-radius: 7px;
 margin-top: 15px;
