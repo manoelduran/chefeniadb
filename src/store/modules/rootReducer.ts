b2d0cedba0ee670a186410f0ec99a298ddf1ed95
Manoel Duran <manoel.duran@hotmail.com>
2023-02-07 21:36:31 -0300

    fix(rootReducer&&rootSagas): removed states

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,14 +1,9 @@
 import { combineReducers } from 'redux';
-import filterMvpReducer from './filterMvp/reducer';
-import mvpReducer from './mvp/reducer';
 import mvpsReducer from './mvps/reducer';
 import roomsReducer from './rooms/reducer';
-import specificMvpsReducer from './specificMvps/reducer';
+
 
 export default combineReducers({
     mvps: mvpsReducer,
-    specificMvps: specificMvpsReducer,
-    mvp: mvpReducer,
     rooms: roomsReducer,
-    filterMvp: filterMvpReducer,
 });
--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -1,13 +1,12 @@
 import { all } from 'redux-saga/effects';
-import mvp from './mvp/sagas';
+
 import mvps from './mvps/sagas';
-import filterMvp from './filterMvp/sagas';
-import specificMvps from './specificMvps/sagas';
+
 import rooms from './rooms/sagas';
 
 
 
 export default function* rootSaga() {
   // return yield all([mvps, specificMvps, mvp, filterMvp, rooms]);
-  return  yield all([mvps, specificMvps, mvp, filterMvp, rooms]);
+  return  yield all([mvps , rooms]);
 }
