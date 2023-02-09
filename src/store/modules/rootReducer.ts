ee12feffe669672d991c85c586878f997414d5b8
Manoel Duran <manoel.duran@hotmail.com>
2023-02-09 00:53:43 -0300

    rootReducer: updated application state

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import mvpsReducer from './mvps/reducer';
 import roomsReducer from './rooms/reducer';
+import userReducer from './user/reducer';
 
 
 export default combineReducers({
     mvps: mvpsReducer,
     rooms: roomsReducer,
+    user: userReducer,
 });
--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -1,12 +1,12 @@
 import { all } from 'redux-saga/effects';
 
 import mvps from './mvps/sagas';
-
+import user from './user/sagas';
 import rooms from './rooms/sagas';
 
 
 
 export default function* rootSaga() {
   // return yield all([mvps, specificMvps, mvp, filterMvp, rooms]);
-  return  yield all([mvps , rooms]);
+  return  yield all([mvps , rooms, user]);
 }
